d1494a2c6f5c20900bdaed454b7e3e43a350cf8a
Jeff Wong <jeffmjwong@gmail.com>
2019-07-15 20:43:00 +1000

    Create logic to upload file to s3 and save url to db in controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,7 +6,23 @@ class MainController < ApplicationController
   end
 
   def upload
-    binding.pry
+    s3 = Aws::S3::Resource.new(
+      region: ENV['S3_REGION'],
+      credentials: Aws::Credentials.new(ENV['ACCESS_KEY_ID'], ENV['SECRET_ACCESS_KEY'])
+    )
+    file = params[:upload_file].tempfile
+    name = params[:upload_file].original_filename
+    s3_object = s3.bucket(ENV['S3_BUCKET']).object(name)
+
+    if s3_object.upload_file(file, acl: 'public-read')
+      AnyFile.create!(
+        url: "https://#{ENV['S3_BUCKET']}.s3-ap-southeast-2.amazonaws.com/#{name}",
+        name: name
+      )
+      redirect_to main_files_path
+    else
+      head 422
+    end
   end
 
   def files
