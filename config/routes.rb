0df97e4d37e0c23536f80c94c7a15b9653e7536a
Jeff Wong <jeffmjwong@gmail.com>
2019-07-15 19:58:58 +1000

    Create route for main_files_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'main#home'
 
-  post '/upload', to: 'main#upload', as: 'main_upload_path'
+  post '/upload', to: 'main#upload', as: 'main_upload'
+  get '/files', to: 'main#files', as: 'main_files'
 end
